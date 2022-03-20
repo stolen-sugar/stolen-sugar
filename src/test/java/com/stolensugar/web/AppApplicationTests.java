ecf673d7459ffb151947459ca3d4800e9db2a773
Ronald Walker <ron22@hey.com>
2022-03-20 07:37:24 -0700

    add getUser test

--- src/test/java/com/stolensugar/web/AppApplicationTests.java
+++ src/test/java/com/stolensugar/web/AppApplicationTests.java
@@ -1,13 +1,21 @@
 package com.stolensugar.web;
 
+import com.stolensugar.web.controller.Controller;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AppApplicationTests {
 
+	@Autowired
+	private Controller controller;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
---
+++ src/test/java/com/stolensugar/web/UserTests.java
@@ -0,0 +1,33 @@
+package com.stolensugar.web;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getUserShouldReturnCorrectUser() throws Exception {
+        String json = "{\"user\":{\"id\":\"1\",\"name\":\"bob\"," +
+                "\"imageavatarUrl\":\"image.com\",\"reposUrl\":\"repos.com\"}}";
+
+        String path = "/users/1";
+
+        this.mockMvc.perform(get(path)).andDo(print()).andExpect(status().isOk())
+                        .andExpect(content().string(containsString(json)));
+    }
+}
+
+
