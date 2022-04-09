41387d3bfd7e778f21c24963a4d0758ae07f0086
Ronald Walker <ron22@hey.com>
2022-04-08 18:16:58 -0700

    revert DataAccessModule

--- src/main/java/com/stolensugar/web/dagger/DataAccessModule.java
+++ src/main/java/com/stolensugar/web/dagger/DataAccessModule.java
@@ -2,8 +2,7 @@ package com.stolensugar.web.dagger;
 
 import javax.inject.Singleton;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -17,17 +16,11 @@ public class DataAccessModule {
     @Singleton
     @Provides
     public DynamoDBMapper provideDynamoDBMapper() {
-        AWSCredentialsProvider awsCredentialsProvider = awsCredentialsProvider();
-
-        AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
-                .withCredentials(awsCredentialsProvider)
+        AmazonDynamoDB amazonDynamoDBClient = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
                 .withRegion(Regions.US_EAST_2)
                 .build();
 
-        return new DynamoDBMapper(amazonDynamoDB);
-    }
-
-    public AWSCredentialsProvider awsCredentialsProvider() {
-        return new EnvironmentVariableCredentialsProvider();
+        return new DynamoDBMapper(amazonDynamoDBClient);
     }
 }
