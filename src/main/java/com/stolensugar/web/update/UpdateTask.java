87d455a57b3ddf6125728cac47002b46951a1ddd
daslater <davidaustinslater@protonmail.com>
2022-04-13 16:17:06 -0500

    added update task

--- src/main/java/com/stolensugar/web/update/UpdateTask.java
+++ src/main/java/com/stolensugar/web/update/UpdateTask.java
@@ -3,25 +3,83 @@ package com.stolensugar.web.update;
 import com.stolensugar.web.dao.SpokenFormDao;
 import com.stolensugar.web.dao.SpokenFormUserDao;
 import com.stolensugar.web.dao.UserDao;
+import org.kohsuke.github.*;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class Updater implements Runnable {
+public class UpdateTask implements Runnable {
 
-    private SpokenFormDao spokenFormDao;
-    private SpokenFormUserDao spokenFormUserDao;
-    private UserDao userDao;
+    private final GitHub github;
+    private final SpokenFormDao spokenFormDao;
+    private final SpokenFormUserDao spokenFormUserDao;
+    private final UserDao userDao;
+
+    private static Date lastUpdated;
 
     @Inject
-    public Updater(SpokenFormDao spokenFormDao, SpokenFormUserDao spokenFormUserDao, UserDao userDao) {
+    public UpdateTask(final GitHub github, final SpokenFormDao spokenFormDao, final SpokenFormUserDao spokenFormUserDao, final UserDao userDao) {
+        this.github = github;
         this.spokenFormDao = spokenFormDao;
         this.spokenFormUserDao = spokenFormUserDao;
         this.userDao = userDao;
+        lastUpdated = new Date();
     }
 
 
     @Override
     public void run() {
 
+        try {
+            updateRepos();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
+
+
+    public void updateRepos() throws IOException {
+        GHRepository baseRepo = github.getRepository("knausj85/knausj_talon");
+        Set<String> baseCommitShas = getCommitShas(baseRepo);
+        List<GHRepository> forks = baseRepo.listForks().toList();
+
+        for (var fork : forks) {
+            List<GHCommit> commits = getCommits(fork, lastUpdated);
+            for (var commit : commits) {
+                if (!baseCommitShas.contains(commit.getSHA1())) {
+
+                }
+            }
+
+        }
+    }
+
+    public Set<String> getCommitShas(GHRepository repo) throws IOException {
+        return repo.queryCommits()
+                .pageSize(100)
+                .list()
+                .toSet()
+                .stream()
+                .map(commit -> commit.getSHA1())
+                .collect(Collectors.toSet());
+    }
+
+    public List<GHCommit> getCommits(GHRepository repo, Date date) throws IOException {
+        List<GHCommit> commits =  repo.queryCommits()
+                .pageSize(100)
+                .since(date)
+                .list()
+                .toList();
+
+        Collections.reverse(commits);
+
+        return commits;
+    }
+
 }
--- src/main/java/com/stolensugar/web/update/Updater.java
+++ src/main/java/com/stolensugar/web/update/Updater.java
@@ -1,2 +1,63 @@
-package com.stolensugar.web.update;public class Updater {
+package com.stolensugar.web.update;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Class responsible for executing update tasks. The updater is created in an off state. A call to start will
+ * start the publishing process.
+ */
+@Singleton
+public class Updater {
+
+    //private static final Logger log = LogManager.getLogger(BookPublisher.class);
+
+    private final ScheduledExecutorService scheduledExecutorService;
+    private final Runnable updateTask;
+    private boolean isRunning;
+
+    /**
+     * Instantiates a new BookPublisher object.
+     *
+     * @param scheduledExecutorService will schedule update tasks
+     * @param updateTask the task that should be scheduled to update the database
+     */
+    @Inject
+    public Updater(ScheduledExecutorService scheduledExecutorService,
+                         Runnable updateTask) {
+        this.updateTask = updateTask;
+        this.scheduledExecutorService = scheduledExecutorService;
+    }
+
+    /**
+     * Start updating
+     */
+    public void start() {
+        if (isRunning) {
+            return;
+        }
+        isRunning = true;
+        scheduledExecutorService.scheduleWithFixedDelay(updateTask, 1, 1, TimeUnit.DAYS);
+    }
+
+    /**
+     * Stop updating
+     */
+    public void stop() {
+        isRunning = false;
+        scheduledExecutorService.shutdown();
+    }
+
+    /**
+     * Returns true if the updater is currently updating and false otherwise.
+     * @return if the updater is currently processing.
+     */
+    @VisibleForTesting
+    boolean isRunning() {
+        return isRunning;
+    }
 }
